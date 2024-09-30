2c2d27d055979c7e743dc775d73e6157125385ee
eduardolongencorrea <eduardolongencorrea@gmail.com>
2024-09-30 14:54:08 -0300

    add delete option

--- src/main/java/com/maxicon/loan_registration_backend/controller/ClientController.java
+++ src/main/java/com/maxicon/loan_registration_backend/controller/ClientController.java
@@ -2,11 +2,14 @@ package com.maxicon.loan_registration_backend.controller;
 
 import com.maxicon.loan_registration_backend.entity.Client;
 import com.maxicon.loan_registration_backend.service.ClientService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -39,4 +42,21 @@ public class ClientController {
     public List<Client> getAllClients() {
         return clientService.getAllClients();
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteClient(@PathVariable Long id) {
+        try {
+            // Perform delete operation
+            clientService.deleteClient(id);
+            // Return success response if deletion is successful
+            return ResponseEntity.ok(Collections.singletonMap("message", "Client deleted successfully"));
+        } catch (IllegalArgumentException e) {
+            // Return a 400 Bad Request with the error message if the client has active loans
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                     .contentType(MediaType.APPLICATION_JSON)
+                     .body(Collections.singletonMap("error", e.getMessage()));
+
+        }
+    }    
+    
 }
